66504f50cd11831197a6e51ff377bb56e180787f
alexander <alexander@applyinnovations.com.au>
2021-10-06 12:24:32 +1100

    made steps more accurate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -927,7 +927,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 79, 1586, 417 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 79, 1591, 417 }
 
 /**
  * Default Max Feed Rate (mm/s)
